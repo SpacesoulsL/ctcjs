78ba9599ec751f324eb11bfa555f3fa0143a44ff
xiaofeng <spacesouls.163.com>
2023-03-31 18:12:57 +0800

    修改

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,11 @@ module.exports =  {
   input: './src/index.js',
   output: [
     {
+      file: 'dist/index.umd.js',
+      name: 'ctcjs',
+      format: 'umd'
+    },
+    {
       file: 'dist/index.es.js',
       format: 'es'
     },
